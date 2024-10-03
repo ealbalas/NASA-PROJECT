dc0d14b3f03384d3f8ea801b1430a698b31644a8
Emad Albalas <ealbalas3@gatech.edu>
2024-10-03 16:25:17 -0700

    created async function in server.js to allow planets data to load and start the server

--- server/src/server.js
+++ server/src/server.js
@@ -5,7 +5,12 @@ const app = require('./app');
 
 const { loadPlanetsData } = require('./models/planets.models');
 
-const server = http.createServer(app);
-server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`);
-});
+async function startServer() {
+    await loadPlanetsData();
+    const server = http.createServer(app);
+    server.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}...`);
+    });
+}
+
+startServer();
