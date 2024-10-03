d3f56fe3987025644c5c5a10e209006df60deea5
Emad Albalas <ealbalas3@gatech.edu>
2024-10-03 16:21:59 -0700

    added model to server.js file

--- server/src/server.js
+++ server/src/server.js
@@ -3,6 +3,8 @@ const PORT = process.env.PORT || 8000;
 const http = require('http');
 const app = require('./app');
 
+const { loadPlanetsData } = require('./models/planets.models');
+
 const server = http.createServer(app);
 server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
