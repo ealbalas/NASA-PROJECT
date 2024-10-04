19e6a8a17971053eb76f0d436165e2e4ef984d9c
Emad Albalas <ealbalas3@gatech.edu>
2024-10-03 22:39:46 -0700

    fixed server to get launches

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,7 @@ const path =require('path');
 const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router');
-const launchsRouter = require('./routes/launches/launches.router.js');
+const launchesRouter = require('./routes/launches/launches.router');
 
 const app = express();
 
@@ -18,10 +18,10 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use(planetsRouter);
-app.use(launchsRouter);
+app.use(launchesRouter);
 
-app.get('/', (res, req) => {
-    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
-})
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
