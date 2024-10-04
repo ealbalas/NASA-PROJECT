66a81aba208a1a6974234226595ef2b5ad323c13
Emad Albalas <ealbalas3@gatech.edu>
2024-10-03 21:44:17 -0700

    added deploy script which runs static files for frontend and starts server file on one server

--- server/src/app.js
+++ server/src/app.js
@@ -12,6 +12,10 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
+
 app.use(planetsRouter);
+app.get('/', (res, req) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+})
 
 module.exports = app;
