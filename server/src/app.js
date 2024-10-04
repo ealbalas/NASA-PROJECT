b284d27552153c117315ce1164b8d40f62af469f
Emad Albalas <ealbalas3@gatech.edu>
2024-10-03 21:29:34 -0700

    have full stack running on one server

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path =require('path');
 
 const planetsRouter = require('./routes/planets/planets.router');
 
@@ -10,6 +11,7 @@ app.use(cors({
     optionsSuccessStatus: 200,
 }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(planetsRouter);
 
 module.exports = app;
