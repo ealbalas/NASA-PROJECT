d517b8d83a2ed3bb96b2128558c8a0ad079bf9b4
Emad Albalas <ealbalas3@gatech.edu>
2024-10-03 21:53:09 -0700

    added some logs using morgan

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path =require('path');
+const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router');
 
@@ -10,6 +11,8 @@ app.use(cors({
     origin: 'http://localhost:3000',
     optionsSuccessStatus: 200,
 }));
+app.use(morgan('combined'));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
