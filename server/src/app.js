66f8c7fc3942d055979a1ff99b51d9bf25b5417a
Emad Albalas <ealbalas3@gatech.edu>
2024-10-03 20:49:36 -0700

    fixed client code (frontend) to accept api

--- server/src/app.js
+++ server/src/app.js
@@ -7,8 +7,9 @@ const app = express();
 
 app.use(cors({
     origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200,
 }));
-app.use(express.json);
+app.use(express.json());
 app.use(planetsRouter);
 
 module.exports = app;
