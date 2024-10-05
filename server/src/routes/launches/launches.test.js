39a3cbf97f4a214ed25dd8df16b853cfcf4b7fad
Emad Albalas <ealbalas3@gatech.edu>
2024-10-04 21:35:07 -0700

    added test-watch in package for server

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,16 @@
+describe('Test GET /launches', () => {
+    test('It should respond with 200 success', () => {
+        const response = 200;
+        expect(response).toBe(200);
+    });
+
+    test('Test POST /launch', () => {
+
+    });
+
+    test('It should catch missing required properties', () => {
+
+    });
+
+    test('It should catch invalid dates', () => {});
+});
