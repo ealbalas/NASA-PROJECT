55ca4ef3ab2925d7f66a7dec5ad334d6f5d22a72
Emad Albalas <ealbalas3@gatech.edu>
2024-10-12 21:25:33 -0700

    needed to add mongodb and node packages to get mongoose working

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,25 @@
-const PORT = process.env.PORT || 8000;
-
 const http = require('http');
+const mongoose = require('mongoose');
 const app = require('./app');
-
 const { loadPlanetsData } = require('./models/planets.models');
+const { error } = require('console');
+
+const PORT = process.env.PORT || 8000;
+const URL = "mongodb+srv://nasa-api:1234@nasacluster.jnyam.mongodb.net/nasa?retryWrites=true&w=majority&appName=NASACluster";
+const server = http.createServer(app);
+
+mongoose.connection.once('open', () => {
+    console.log('MongoDB conncection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error(error);
+});
+
 
 async function startServer() {
+    await mongoose.connect(URL);
     await loadPlanetsData();
-    const server = http.createServer(app);
     server.listen(PORT, () => {
         console.log(`Listening on port ${PORT}...`);
     });
